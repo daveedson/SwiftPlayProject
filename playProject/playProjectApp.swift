dc5d636883506ac2d23243115d18ccd2a3a218a3
David Onoh <davidonoh@gmail.com>
2023-11-04 17:17:27 +0100

    Initial Commit

---
+++ playProject/playProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  playProjectApp.swift
+//  playProject
+//
+//  Created by David Onoh on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct playProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
